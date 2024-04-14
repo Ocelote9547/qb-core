5c43d67acbef4e298a10542473bdf54277e3ff1f
Ocelote9547 <142257083+Ocelote9547@users.noreply.github.com>
2024-04-14 17:19:28 +0200

    Update qbcore2.sql

--- qbcore2.sql
+++ qbcore2.sql
@@ -17,12 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `s144415_RedvsBlue`
---
-CREATE DATABASE IF NOT EXISTS `s144415_RedvsBlue` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `s144415_RedvsBlue`;
-
 -- --------------------------------------------------------
 
 --
